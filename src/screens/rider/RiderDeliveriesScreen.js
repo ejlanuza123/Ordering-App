c9e79bd92d58732c50f8cc80f05103d0a54d7764
ejlanuza0123 <ejlanuza0123@gmail.com>
2026-08-11 10:07:34 +0800

    fix(rider): restore missing closing parenthesis in RiderDeliveriesScreen.js
    - Fixes Babel bundling syntax error in RiderDeliveriesScreen.js
    - Bump version 1.12.0 -> 1.12.1

--- src/screens/rider/RiderDeliveriesScreen.js
+++ src/screens/rider/RiderDeliveriesScreen.js
@@ -313,6 +313,7 @@ export default function RiderDeliveriesScreen({ navigation, route }) {
             </View>
           }
         />
+      )}
       {/* GPS Navigation Modal */}
       <GPSNavigationModal
         visible={!!navDestination}
